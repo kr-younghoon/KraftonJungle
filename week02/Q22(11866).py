fe8b814dadd87b5923c3c573bc77089840051c51
younghoon <sullanta0802@gmail.com>
2023-04-18 22:27:56 +0900

    화 22:28

--- week02/Q22(11866).py
+++ week02/Q22(11866).py
@@ -1,3 +1,24 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
+N, K = map(int, input().split())
+original = deque(x+1 for x in range(N))
+final = []
+
+
+while True:
+    for i in range(1, K):
+        original.append(original.popleft())
+    final.append(original.popleft())
+    if len(original) < 1:
+        break
+
+print("<", end="")
+for i in final:
+    if i != final[-1]:
+        print(i, end=", ")
+    else:
+        print(i, end="")
+print(">")
+    
