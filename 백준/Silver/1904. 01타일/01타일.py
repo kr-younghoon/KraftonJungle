0edd47acafe7cbebaa251cd03d4cd518738fd039
이영훈 <126774519+kr-younghoon@users.noreply.github.com>
2023-04-28 16:28:05 +0900

    [Silver III] Title: 01타일, Time: 168 ms, Memory: 191892 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,10 +1,8 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-
-# 초기값 설정
-dp = [0, 1, 2]
-
-# 동적 계획법을 이용하여 문제 해결
-for i in range(3, N+1):
-    dp.append((dp[i-1] + dp[i-2]) % 15746)
-
-print(dp[N])
+tile = [1, 2]
+for i in range(2, N):
+    tile.append((tile[i-1] + tile[i-2]) % 15746)
+print(tile[N-1])
