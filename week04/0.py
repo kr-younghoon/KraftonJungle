eab120a45f588cc0ccc86b897c2f7382c3136c42
younghoon <sullanta0802@gmail.com>
2023-05-01 17:59:14 +0900

    월 18:00

--- week04/0.py
+++ week04/0.py
@@ -1,23 +1,28 @@
-import sys
+# # ex | 55 - 50 + 40
 
-input = sys.stdin.readline
+# in_line = input().split('-')  # ["55", "50 + 40"]
+# num = [] # 리스트 선언
+# for i in in_line: # for "55", "50 + 40" - 2 times
+#     cnt = 0 # count 
+#     s = i.split("+") # 55, 50, 40?
+#     for j in s: # 55, 50, 40
+#         cnt += int(j) 
+#     num.append(cnt)
+# n = num[0]
+# for i in range(1, len(num)):
+#     n -= num[i]
+# print(n)
 
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    coins = list(map(int, input().split()))
-    m = int(input())
 
-    # memoization을 위한 리스트 선언
-    d = [0] * (m + 1)
-    d[0] = 1
-
-
-    for coin in coins:
-        for i in range(m + 1):
-            # a_(i-k) 를 만드는 방법이 존재한다면 
-            # 이전 경우의 수에 현재 동전으로 만들 수 있는 경우의 수를 더한다.
-            if i >= coin:
-                d[i] += d[i - coin]
-
-    print(d[m])
+in_line = ["55", "50 + 40"]
+for i in in_line:
+    cnt = 0
+    print(f"19line - call (i) :: {i}")
+    s = i.split("+")
+    print(f"21line - call (s) :: {s}")
+    
+    for j in s:
+        print(f"23line - call (j) :: {j}")
+        
+        cnt += int(j)
+    print(cnt)
