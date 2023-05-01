1813b3d671592c7cfb0885d80302986ac2bbc8a2
이영훈 <126774519+kr-younghoon@users.noreply.github.com>
2023-05-01 21:38:45 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 131636 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,5 +1,6 @@
 N = int(input())
-check_list = sorted([list(map(int, input().split())) for _ in range(N)], key= lambda x: (x[1], x[0]))
+check_list = [list(map(int, input().split())) for _ in range(N)]
+check_list.sort(key= lambda x: (x[1], x[0]))
 cnt = end = 0
 for s, e in check_list:
     if s >= end:
